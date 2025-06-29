4775458eade3bc24a9b7600f10788fc7008596f1
rt777-333 <tribka7@gmail.com>
2025-06-29 18:21:54 +0300

    Create script.js

---
+++ dom-manipulation/script.js
@@ -0,0 +1,12 @@
+  <input type="file" id="importFile" accept=".json" onchange="importFromJsonFile(event)" />
+
+  function importFromJsonFile(event) {
+    const fileReader = new FileReader();
+    fileReader.onload = function(event) {
+      const importedQuotes = JSON.parse(event.target.result);
+      quotes.push(...importedQuotes);
+      saveQuotes();
+      alert('Quotes imported successfully!');
+    };
+    fileReader.readAsText(event.target.files[0]);
+  }
